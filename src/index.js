92e356fccd9e1c0380d99020787d98b1c850ebd3
Aakash Sethi <aakashsethi20@gmail.com>
2022-01-08 17:11:26 -0500

    convert Square to function component; add next turn state

--- src/index.js
+++ src/index.js
@@ -2,38 +2,39 @@ import { React, Component } from "react";
 import { render } from "react-dom";
 import './index.css';
 
-class Square extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.props.onClick()}
-      >
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null)
+      squares: Array(9).fill(null),
+      xIsNext: true
     }
+  }
+
+  getNextTurn() {
+    return this.state.xIsNext ? 'X' : 'O';
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.getNextTurn();
+    this.setState(
+      {
+        squares: squares,
+        xIsNext: !this.state.xIsNext,
+      }
+    );
   }
 
   renderSquare(i) {
@@ -46,7 +47,7 @@ class Board extends Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + this.getNextTurn();
 
     return (
       <div>
